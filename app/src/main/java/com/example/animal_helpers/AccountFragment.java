12a7724830856f908133f00b121ebb4ca9892b83
uryd <fjthfo@gmail.com>
2023-07-16 21:30:39 +0900

    AccountFragment 프로필 업로드(Firebase Storage) 기능 추가

--- app/src/main/java/com/example/animal_helpers/AccountFragment.java
+++ app/src/main/java/com/example/animal_helpers/AccountFragment.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -29,65 +28,64 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.util.Objects;
+
 
 public class AccountFragment extends Fragment {
 
     FirebaseStorage storage = null;
     Uri uri = null;
     ImageView iv_profile;
-    Button select, view;
-    FirebaseAuth mAuth;
+//    Button select, view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_account, container, false);
 
-
+//        select = (Button) v.findViewById(R.id.select);
+//        view = (Button) v.findViewById(R.id.view);
         iv_profile = (ImageView) v.findViewById(R.id.iv_profile);
-        select = (Button) v.findViewById(R.id.select);
-        view = (Button) v.findViewById(R.id.view);
         storage = FirebaseStorage.getInstance();
         String Uid = FirebaseAuth.getInstance().getUid();
 
-        iv_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT );
-                intent.setType("image/*");
-                launcher.launch(intent);
-            }
+        show(Uid);
+
+        iv_profile.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT );
+            intent.setType("image/*");
+            launcher.launch(intent);
         });
 
-        select.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                upload(Uid);
-
-            }
-        });
-
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                show(Uid);
-
-            }
-        });
+//        view.setOnClickListener(view -> show(Uid));
+//        select.setOnClickListener(view -> upload(Uid));
 
         return v;
     }
 
     private final ActivityResultLauncher<Intent> launcher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
+            new ActivityResultCallback<>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
-                    if(result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    String Uid = FirebaseAuth.getInstance().getUid();
+                    StorageReference storageReference = FirebaseStorage.getInstance().getReference("Animal-helper");
+
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
                         uri = result.getData().getData();
                         Log.d("test", uri.toString());
-
+                        Glide.with(requireActivity()).load(uri).circleCrop().into(iv_profile);
                     }
+                    storageReference.child(Objects.requireNonNull(Uid)).child("img_profile").putFile(uri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getActivity(), "업로드에 성공했습니다", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getActivity(), "업로드에 실패했습니다", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
             });
 
@@ -111,8 +109,9 @@ public class AccountFragment extends Fragment {
         storageReference.child(Uid).child("img_profile").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
-                Glide.with(requireActivity()).load(uri).into(iv_profile);
+                Glide.with(requireActivity()).load(uri).circleCrop().into(iv_profile);
             }
+
         });
     }
 }
